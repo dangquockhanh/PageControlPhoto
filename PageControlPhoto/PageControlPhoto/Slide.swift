045c4cc8c71bd4a727136984d26110ecb08ab755
Khánh Trắng <harry.edward456@gmail.com>
2019-05-28 16:40:03 +0700

    Done

---
+++ PageControlPhoto/PageControlPhoto/Slide.swift
@@ -0,0 +1,18 @@
+//
+//  Slide.swift
+//  PageControlPhoto
+//
+//  Created by Đặng Khánh  on 5/28/19.
+//  Copyright © 2019 Khánh Trắng. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var labelTitle: UILabel!
+    
+    @IBOutlet weak var labelDesc: UILabel!
+}
